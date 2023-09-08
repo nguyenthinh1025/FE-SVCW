1d6d8d2f735508c64a6b707bab06a625952d9200
nguyenphuthinh1025 <nguyenphuthinh1025@gmail.com>
2023-09-08 22:05:26 +0700

    create result

--- src/utils/reponse.js
+++ src/utils/reponse.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 export const http = axios.create({
-  baseURL: 'https://svcw-system.azurewebsites.net/api',
+  baseURL: 'https://localhost:7083/api',
   // https://svcw-system.azurewebsites.net/api
   // timeout: 3000
 });
